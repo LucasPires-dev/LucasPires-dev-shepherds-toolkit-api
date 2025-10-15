1f091fffe49f699d5dda7c1bbcccda66fe58faef
Lucas C. Pires <lcpires7.dev@gmail.com>
2025-10-15 06:41:08 -0300

    upload

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = 'django-insecure-c#l#cgo0va@sd@vm7!c=b1m%(d0h7xkd44%%7ohrqd_ei%5*b(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://shepherds-toolkit-api-production.up.railway.app/']
+ALLOWED_HOSTS = [
+    'https://shepherds-toolkit-api-production.up.railway.app/',
+    '.railway.app',
+    'localhost',
+]
 
 # Application definition
 
